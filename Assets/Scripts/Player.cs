25a34f0865fa0c84f82c27589806e285dbdc1a33
Gabriel Leal <gabriel-pedro-leal@hotmail.com>
2026-03-06 15:12:49 -0300

    Adicionar arquivos de projeto.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+
+    private Rigidbody2D rig;
+
+    private float initialSpeed;
+    private bool _isRunning;
+    private Vector2 _direction;
+
+    public Vector2 direction
+    {
+        get {  return _direction; }
+        set { _direction = value; }
+    }
+
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
+    }
+
+    private void Update()
+    {
+        OnInput();
+        OnRun();
+    }
+
+    private void FixedUpdate()
+    {
+        OnMove();
+
+    }
+
+    #region Movement
+
+    private void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void OnMove()
+    {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+            _isRunning = true;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+    #endregion
+}
---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{
+    private Player player;
+    private Animator anim;
+
+    private void Start()
+    {
+        player = GetComponent<Player>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        OnMove();
+        OnRun();
+    }
+
+    #region Movement
+
+    private void OnMove()
+    {
+        if (player.direction.sqrMagnitude > 0)
+        {
+            anim.SetInteger("transition", 1);
+        }
+        else
+        {
+            anim.SetInteger("transition", 0);
+        }
+
+        if (player.direction.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1);
+        }
+        else if (player.direction.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1);
+        }
+    }
+
+    private void OnRun()
+    {
+        if(player.isRunning)
+        {
+            anim.SetInteger("transition", 2);
+        }
+    }
+
+    #endregion  
+}
